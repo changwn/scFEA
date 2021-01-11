bc142dcb6666b69353c2119ea33f12273c7ac2d8
changwn <changwn.nku@gmail.com>
2021-01-11 18:27:34 -0500

    update main function

--- src/scFEA.py
+++ src/scFEA.py
@@ -6,116 +6,108 @@
 
 # system lib
 import argparse
-import sys
 import time
-import os
 
 # tools
 import torch
 from torch.autograd import Variable
-import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
-from torch.utils.data import DataLoader
-import torch.nn as nn
+import magic
 
-
-# Flux lib
+# scFEA lib
 from ClassFlux import FLUX # Flux class network
+from util import pearsonr
+from DatasetFlux import MyDataset
 
 
 # hyper parameters
-BATCH_SIZE = 1
 LEARN_RATE = 0.008  # 0.008 for m14 ; 0.02 for m100, 0.01 for m100 162 cell.
 EPOCH = 100
 LAMB_BA = 1
-LAMB_SCALE = 2
-LAMB_SIMI = 0
+LAMB_NG = 1 
+LAMB_CELL =  1
+LAMB_MOD = 1e-2 
 
 
-def myLoss(m,  c, df, lamb1 = 0.2, lamb2= 0.2, lamb3 = 0.2, cellId = 1):    
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def myLoss(m, c, df, mLength, lamb1 = 0.2, lamb2= 0.2, lamb3 = 0.2, lamb4 = 0.2, cellId = 1, superIDtable = None, superModuleID = None, geneScale = None, moduleScale = None):    
     
-    #print(c)
-    #print(m.shape)
+    # balance constrain
     total1 = torch.pow(c, 2)
-    total1 = torch.sum(total1, dim = 1) #c -> 0.
-#    total1 = torch.pow(total1, 0.5)
+    total1 = torch.sum(total1, dim = 1) 
     
-    df.index = [i.split('_')[0] for i in df.index]
-    T = df.groupby(df.index).sum(axis=1).T
-#    print(T.values[:,1])
+    # non-negative constrain
+    error = torch.abs(m) - m
+    total2 = torch.sum(error, dim=1)   
     
-       
-    #module 14 constrain
-#    error_a = (m[:,4] - torch.FloatTensor(T.values[:,1])).to(device)
-#    error_a = torch.pow(error_a, 2)
-#    error_b = (m[:,1] - torch.FloatTensor(T.values[:,1])).to(device)
-#    error_b = torch.pow(error_b, 2)
-#    error = error_a + error_b
+    # sample-wise variation constrain 
+    corr = pearsonr(torch.mean(m, dim=1), geneScale)
+    penal_var = torch.FloatTensor(np.ones(1)) - corr
+    total3 = penal_var
     
-#    #module 100 constrain
-    error = (m - torch.FloatTensor(T.values)).to(device)
-    error = torch.pow(error, 2)
-    error = torch.sum(error, dim=1)/10
-    error = torch.pow(error, 0.5)
-
-    
-    total2 = error
+    # module-wise variation constrain
+    corr = torch.FloatTensor(np.ones(m.shape[0]))
+    for i in range(m.shape[0]):
+        corr[i] = pearsonr(m[i, :], moduleScale[i, :])
+    corr = torch.abs(corr)
+    penal_m_var = torch.FloatTensor(np.ones(m.shape[0])) - corr
+    total4 = penal_m_var
             
-    total = lamb1 * total1 + lamb2 * total2 
-    
-    loss1 = torch.sum(total1)
+    # loss
+    loss1 = torch.sum(lamb1 * total1)
     loss2 = torch.sum(lamb2 * total2)
-    loss = torch.sum(total) 
-    return loss1, loss2, loss
-
+    loss3 = torch.sum(lamb3 * total3)
+    loss4 = torch.sum(lamb4 * total4)
+    loss = loss1 + loss2 + loss3 + loss4
+    return loss, loss1, loss2, loss3, loss4
+    
 
 def main(args):
 
-    
-	# set arguments
+    # set arguments
     data_path = args.data_dir
     res_dir = args.res_dir
     test_file = args.test_file
     moduleGene_file = args.moduleGene_file
     cm_file = args.stoichiometry_matrix
-    
+    sc_imputation = args.sc_imputation
+
+
     # choose cpu or gpu automatically
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     # read data
-#    os.chdir('your_current_directory')
     geneExpr = pd.read_csv(
-#                "./data/Melissa_metabolic_c88_m14.csv",
-				data_path + '/' + test_file,	
+                data_path + '/' + test_file,
                 index_col=0)
+    if sc_imputation == True:
+        magic_operator = magic.MAGIC()
+        geneExpr = magic_operator.fit_transform(geneExpr)
     geneExpr = geneExpr.T
-    if geneExpr.max().max() > 30:
-        geneExpr = (geneExpr + 1).apply(np.log2)  #GSE103322 no log2
+    if geneExpr.max().max() > 50:
+        geneExpr = (geneExpr + 1).apply(np.log2)  
     geneExprSum = geneExpr.sum(axis=1)
     stand = geneExprSum.mean()
     geneExprScale = geneExprSum / stand
     geneExprScale = torch.FloatTensor(geneExprScale.values).to(device)
     
+    BATCH_SIZE = geneExpr.shape[0]
     
     moduleGene = pd.read_csv(
-#                "./data/module_gene_m14.csv",
-				data_path + '/' + moduleGene_file, 
+                data_path + '/' + moduleGene_file,
                 sep=',',
-                #header=None,
                 index_col=0)
-#    moduleGene = moduleGene.replace(np.nan, '', regex=True)
+    moduleLen = [moduleGene.iloc[i,:].notna().sum() for i in range(moduleGene.shape[0]) ]
+    moduleLen = np.array(moduleLen)
     
     cmMat = pd.read_csv(
-#            "./data/cmMat_m14.csv",
-			data_path + '/' + cm_file,
+            data_path + '/' + cm_file,
             sep=',',
             header=None)
     cmMat = cmMat.values
     cmMat = torch.FloatTensor(cmMat).to(device)
-
+    
     
     emptyNode = []
     gene_names = geneExpr.columns
@@ -135,11 +127,19 @@ def main(args):
         temp.loc[:, [g for g in gene_names if g not in genes]] = 0
         temp = temp.T
         temp['Module_Gene'] = ['%02d_%s' % (i,g) for g in gene_names]
-        geneExprDf = geneExprDf.append(temp, ignore_index = True)
+        geneExprDf = geneExprDf.append(temp, ignore_index = True, sort=False)
     geneExprDf.index = geneExprDf['Module_Gene']
     geneExprDf.drop('Module_Gene', axis = 'columns', inplace = True)
     X = geneExprDf.values.T
     X = torch.FloatTensor(X).to(device)
+    
+    #prepare data for constraint of module variation based on gene
+    df = geneExprDf
+    df.index = [i.split('_')[0] for i in df.index]
+    df.index = df.index.astype(int)   # mush change type to ensure correct order, T column name order change!
+    #module_scale = df.groupby(df.index).sum(axis=1).T   # pandas version update
+    module_scale = df.groupby(df.index).sum().T  
+    module_scale = torch.FloatTensor(module_scale.values/ moduleLen) 
 
     
     
@@ -147,60 +147,49 @@ def main(args):
     #NN
     torch.manual_seed(16)
     net = FLUX(X, n_modules, f_in = n_genes, f_out = 1).to(device)
-#    print(net.m_encoder[0].parameters())
     optimizer = torch.optim.Adam(net.parameters(), lr = LEARN_RATE)
-#    optimizer = torch.optim.Adam(net.m_encoder[0].parameters(), lr = LEARN_RATE)
-#    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=10, verbose=True, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08)
 
     #Dataloader
     dataloader_params = {'batch_size': BATCH_SIZE,
                          'shuffle': False,
                          'num_workers': 0,
                          'pin_memory': False}
-    train_loader = torch.utils.data.DataLoader(
-                                                torch.utils.data.TensorDataset(X),
-                                                **dataloader_params)
+
+    dataSet = MyDataset(X, geneExprScale, module_scale)
+    train_loader = torch.utils.data.DataLoader(dataset=dataSet,
+                                               **dataloader_params)
     
 # =============================================================================
 
   
-    
-    # initialize
-#    def init_weights(w):
-#        if type(w) == torch.nn.Linear:
-#            torch.nn.init.xavier_uniform(w.weight, gain = 10)
-#            w.bias.data.fill_(0.01)
-#    net.apply(init_weights)
    
     
 # =============================================================================
     start = time.time()  
-#     training
-    lamb_ba = LAMB_BA
-    lamb_scale = LAMB_SCALE
-    lamb_simi = LAMB_SIMI
+#   training
     loss_v = []
     loss_v1 = []
     loss_v2 = []
+    loss_v3 = []
+    loss_v4 = []
     net.train()
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    lossName = "./output/lossValue_" + timestr + ".txt"
+    file_loss = open(lossName, "a")
     for epoch in range(EPOCH):
-        loss, loss1, loss2 = 0,0,0
+        loss, loss1, loss2, loss3, loss4 = 0,0,0,0,0
         
-        for i, item in enumerate(train_loader): # 162
-            
-            X_batch = Variable(item[0])
-            out_m_batch, out_c_batch = net(X_batch, n_modules, n_genes, n_comps, cmMat, geneExprScale, i)
+        for i, (X, X_scale, m_scale) in enumerate(train_loader):
 
-#            if i == 0:
-#                out_m = out_m_batch
-#                out_c = out_c_batch
-#            else:
-#                out_m = torch.cat((out_m, out_m_batch), dim = 0)
-#                out_c = torch.cat((out_c, out_c_batch), dim = 0)
-               
-            X_batch_df = pd.DataFrame(X_batch.cpu().data.numpy().T, index = geneExprDf.index)
-            loss1_batch, loss2_batch, loss_batch = myLoss(out_m_batch, out_c_batch, X_batch_df, lamb1 = lamb_ba, lamb2 = lamb_scale, lamb3 = lamb_simi, cellId = i)
-            # back propagation
+            X_batch = Variable(X.float().to(device))
+            X_scale_batch = Variable(X_scale.float().to(device))
+            m_scale_batch = Variable(m_scale.float().to(device))
+            
+            out_m_batch, out_c_batch = net(X_batch, n_modules, n_genes, n_comps, cmMat)
+            loss_batch, loss1_batch, loss2_batch, loss3_batch, loss4_batch  = myLoss(out_m_batch, out_c_batch, 
+                                                                                     lamb1 = LAMB_BA, lamb2 = LAMB_NG, lamb3 = LAMB_CELL, lamb4 = LAMB_MOD, 
+                                                                                     geneScale = X_scale_batch, moduleScale = m_scale_batch)
+
             optimizer.zero_grad()
             loss_batch.backward()
             optimizer.step()
@@ -208,70 +197,89 @@ def main(args):
             loss += loss_batch.cpu().data.numpy()
             loss1 += loss1_batch.cpu().data.numpy()
             loss2 += loss2_batch.cpu().data.numpy()
+            loss3 += loss3_batch.cpu().data.numpy()
+            loss4 += loss4_batch.cpu().data.numpy()
             
-#        scheduler.step(loss)
-#        print("m...", out_m_batch[0,:])
-#        print("c*********", out_c_batch[0,:])
-#        print('epoch: %02d, loss1: %.8f, loss2_lambda_free: %.8f, loss: %.8f' % (epoch+1, loss1, loss2/lamb_scale, loss))
-        if(epoch % 5 == 0):
-            print('epoch:', epoch)
+        print('epoch: %02d, loss1: %.8f, loss2: %.8f, loss3: %.8f, loss4: %.8f, loss: %.8f' % (epoch+1, loss1, loss2, loss3, loss4, loss))
+        file_loss.write('epoch: %02d, loss1: %.8f, loss2: %.8f, loss3: %.8f, loss4: %.8f, loss: %.8f. \n' % (epoch+1, loss1, loss2, loss3, loss4, loss))
+        
         loss_v.append(loss)
         loss_v1.append(loss1)
-        loss_v2.append(loss2/lamb_scale)
+        loss_v2.append(loss2)
+        loss_v3.append(loss3)
+        loss_v4.append(loss4)
         
 # =============================================================================
     end = time.time()
     print("Training time: ", end - start) 
     
-    plt.plot(loss_v)
+    file_loss.close()
+    plt.plot(loss_v, '--')
     plt.plot(loss_v1)
     plt.plot(loss_v2)
-    plt.legend(['total', 'balance', 'scale']);
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-#    imgName = "./output/loss_" + timestr + ".png"
+    plt.plot(loss_v3)
+    plt.plot(loss_v4)
+    plt.legend(['total', 'balance', 'negative', 'cellVar', 'moduleVar']);
     imgName = './' + res_dir + '/loss_' + timestr + ".png"
     plt.savefig(imgName)
-    
-    #save model
-#    torch.save(net, './output/model_Melissa_m14_c88' + timestr + '.pkl')
-    
+    timeName =  './' + res_dir + '/time_' + timestr + ".txt"
+    f = open(timeName, "a")
+    runTimeStr = str(end - start)
+    f.write(runTimeStr)
+    f.close()   
+
     
 #    Dataloader
     dataloader_params = {'batch_size': 1,
                          'shuffle': False,
                          'num_workers': 0,
                          'pin_memory': False}
-    test_loader = torch.utils.data.DataLoader(
-                                                torch.utils.data.TensorDataset(X),
-                                                **dataloader_params)
+
+    dataSet = MyDataset(X, geneExprScale, module_scale)
+    test_loader = torch.utils.data.DataLoader(dataset=dataSet,
+                          **dataloader_params)
    
     #testing
     fluxStatuTest = np.zeros((n_cells, n_modules), dtype='f') #float32
+    balanceStatus = np.zeros((n_cells, n_comps), dtype='f')
     net.eval()
     for epoch in range(1):
         loss, loss1, loss2 = 0,0,0
-        for i, item in enumerate(test_loader): # 162
-            #print(i)
-            #optimizer.zero_grad()
-            X_batch = Variable(item[0])
-            out_m_batch, out_c_batch = net(X_batch, n_modules, n_genes, n_comps, cmMat, geneExprScale, i)
+        
+        for i, (X, X_scale, _) in enumerate(test_loader):
+
+            X_batch = Variable(X.float().to(device))
+            out_m_batch, out_c_batch = net(X_batch, n_modules, n_genes, n_comps, cmMat)
             
             # save data
             fluxStatuTest[i, :] = out_m_batch.detach().numpy()
+            balanceStatus[i, :] = out_c_batch.detach().numpy()
             
-            
+                   
     
     # save to file
-    fileName = "./" + res_dir + "/module" + str(n_modules) + "_cell" + str(n_cells) + "_batch" + str(BATCH_SIZE) + "_LR" + str(LEARN_RATE) + "_lambSca" + str(LAMB_SCALE) + "_" + timestr + ".csv"
+    fileName = "./" + res_dir + "/module" + str(n_modules) + "_cell" + str(n_cells) + "_batch" + str(BATCH_SIZE) + \
+                "_LR" + str(LEARN_RATE) + "_epoch" + str(EPOCH) + "_SCimpute_" + str(sc_imputation)[0] + \
+                "_lambBal" + str(LAMB_BA) + "_lambSca" + str(LAMB_NG) + "_lambCellCor" + str(LAMB_CELL) + "_lambModCor_1e-2" + \
+                '_' + timestr + ".csv"
     setF = pd.DataFrame(fluxStatuTest)
     setF.columns = moduleGene.index
     setF.index = geneExpr.index.tolist()
     setF.to_csv(fileName)
     
+    setB = pd.DataFrame(balanceStatus)
+    setB.rename(columns = lambda x: x + 1)
+    setB.index = setF.index
+    balanceName = "./output/balance_" + timestr + ".csv"
+    setB.to_csv(balanceName)
+    
+
     print("Done. Check result in the desired output folder.")
     
+    
     return
-	
+
+
 def parse_arguments(parser):
 
 
@@ -279,12 +287,14 @@ def parse_arguments(parser):
                         help='The data directory for input data')
     parser.add_argument('--res_dir', type=str, default='output', metavar='<data_directory>',
                         help='The data directory for result [output]')
-    parser.add_argument('--test_file', type=str, default='Melissa_metabolic_c88_m14.csv', help='The test SC file [input]')
-    parser.add_argument('--moduleGene_file', type=str, default='module_gene_m14.csv', 
-						help='The table contains genes for each module.')
-    parser.add_argument('--stoichiometry_matrix', type=str, default='cmMat_m14.csv', 
-						help='The table describes relationship between compounds and modules.')
-
+    parser.add_argument('--test_file', type=str, default='Melissa_full.csv', 
+                        help='The test SC file [input]')
+    parser.add_argument('--moduleGene_file', type=str, default='module_gene_m171_vDec2020.csv', 
+                        help='The table contains genes for each module.')
+    parser.add_argument('--stoichiometry_matrix', type=str, default='cmMat_m171.csv', 
+                        help='The table describes relationship between compounds and modules.')
+    parser.add_argument('--sc_imputation', type=eval, default='True', choices=[True, False],
+                        help='Whether perform imputation for SC dataset (recommend set to <True> for 10x data).')
 
     args = parser.parse_args()
 
